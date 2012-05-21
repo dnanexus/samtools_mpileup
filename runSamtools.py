72415f555355f89b2f8720ff6dd39b6a8751b77d
andrew carroll <acarroll@dnanexus.com>
2012-05-21 11:50:19 -0700

    Switch to use vcf import utiltiy script

--- runSamtools.py
+++ runSamtools.py
@@ -14,10 +14,6 @@ def main():
     mappingsTable = dxpy.open_dxgtable(job['input']['mappings']['$dnanexus_link'])
     mappingsTableId = mappingsTable.get_id()
     try:
-        ##NEED TO UPDATE TO NEW CONTIGSET SPELLING AFTER TESTING DONE
-        #contigSetId = mappingsTable.get_details()['originalContigSet']['$dnanexus_link']
-        #originalContigSet = mappingsTable.get_details()['originalContigSet']
-        
         contigSetId = mappingsTable.get_details()['original_contigset']['$dnanexus_link']
         originalContigSet = mappingsTable.get_details()['original_contigset']
     except:
@@ -80,7 +76,7 @@ def main():
             reduceInput["mapJob" + str(i) + "TableId"] = {'job': mapJobId, 'field': 'id'}
 
     reduceInput['tableId'] = tableId
-    reduceJobId = dxpy.new_dxjob(fn_input=reduceInput, fn_name="reduceGatk").get_id()
+    reduceJobId = dxpy.new_dxjob(fn_input=reduceInput, fn_name="reducePileup").get_id()
     #print "SimpleVar table" + json.dumps({'table_id':simpleVar.get_id()})
     job['output'] = {'simplevar': {'job': reduceJobId, 'field': 'simplevar'}}
 
@@ -162,11 +158,19 @@ def mapPileup():
     print command
     subprocess.call(command, shell=True)
     
-    parseVcf(open("output.vcf", 'r'), simpleVar, job['input']['compress_reference'], job['input']['compress_no_call'], job['input']['store_full_vcf'])
-    
+
+    command = "dx_vcfToSimplevar --table_id %s --vcf_file output.vcf" % (job['input']['tableId'])
+    if job['input']['compress_reference']:
+        command += " --compress_reference"
+    if job['input']['compress_no_call']:
+        command += " --compress_no_call"
+    if job['input']['store_full_vcf']:
+        command += " --store_full_vcf"
     if job['input']['part_number'] == 0:
-        header = extractHeader(open("output.vcf", 'r'))
-        simpleVar.set_details({'header':header, 'original_contigset':dxpy.dxlink(job['input']['original_contig_set'])})
+        command += " --extract_header"
+    print command    
+    subprocess.call(command ,shell=True)
+    
 
 
 def runTrivialTest(contig_set, command):
@@ -203,224 +207,12 @@ def extractHeader(vcfFile):
         except StopIteration:
             break
     
-def reduceGatk():
+def reducePileup():
     t = dxpy.open_dxgtable(job['input']['tableId'])
     t.close(block=True)
     print "Closing Table"
     job['output']['simplevar'] = dxpy.dxlink(t.get_id())
     
-def parseVcf(vcfFile, simpleVar, compressNoCall, compressReference, storeFullVcf):
-
-    #These prior variables are used for keeping track of contiguous reference/no-call
-    #   in the event that compressReference or compressNoCall is True
-    priorType = "None"
-    priorPosition = -1
-
-    fileIter = vcfFile.__iter__()
-    count = 1
-
-    #Additional data will contain the extra format and info columns that are optional in VCF and may not be
-    #   present in the VCF file. These are stored in an extended table 
-    additionalData = []
-    
-    while 1:
-        try:
-            input = fileIter.next()
-            if count%100000 == 0:
-                print "Processed count %i variants " % count
-            count += 1
-            
-            if input[0] != "#":
-                tabSplit = input.strip().split("\t")
-                chr = tabSplit[0]
-                lo = int(tabSplit[1])
-                hi = lo + len(tabSplit[3])
-                ref = tabSplit[3].replace(".","")
-                
-                #In VCF format, the ALT column holds possible candidate alleles. The actual call as to the
-                #   variant and its zygosity is a combination of ALT and the genotype specified in the info field.
-                #   We store all of the options (including ref) and calculated the actual calls later
-                altOptions = [ref]
-                altOptions.extend(tabSplit[4].split(","))
-                qual = tabSplit[5]
-                type = "Unknown"
-                if qual == ".":
-                    type = "No-call"
-                else:
-                    qual = int(float(tabSplit[5]))
-
-                formatColumn = tabSplit[7]
-                infoColumn = tabSplit[8]
-                genotypeQuality = 0
-                
-                coverage = re.findall("DP=(\d+);", formatColumn)
-                if(len(coverage) > 0):
-                    coverage = int(coverage[0])
-                else:
-                    coverage = 0
-
-                if altOptions == [ref, '.']:
-                    if type == "No-call":
-                        if compressNoCall == False:
-                            entry = [chr, lo, hi, type, "", "", 0, 0, 0]
-                            entry.append(tabSplit[4])
-                            vcfSpecificData = ''
-                            for x in tabSplit[7:]:
-                                vcfSpecificData += x+"\t"
-                            entry.append(tabSplit[7:].strip())
-                    else:
-                        type = "Ref"
-                        if compressReference == False:
-                            entry = [chr, lo, hi, type, "", "", 0, 0, 0]
-                            entry.append(tabSplit[4])
-                            vcfSpecificData = ''
-                            for x in tabSplit[7:]:
-                                vcfSpecificData += x+"\t"
-                            entry.append(vcfSpecificData.strip())
-                else:
-                    #In VCF format, the prior character to a sequence change is given in some cases (Ins, Del)
-                    #   we are removing this in our format, and so need to figure out which characters to filter   
-                    overlap = findMatchingSequence(ref, altOptions, False)
-                    ref = ref[overlap:]
-                    for i in range(len(altOptions)):
-                        altOptions[i] = altOptions[i][overlap:]
-                    reverseOverlap = findMatchingSequence(ref, altOptions, True)
-
-                    ref = ref[:len(ref)-reverseOverlap]
-                    for i in range(len(altOptions)):
-                        altOptions[i] = altOptions[i][:len(altOptions[i])-reverseOverlap]
-                    
-                    
-                    
-                    #Find all of the genotypes 
-                    genotypePossibilities = {}
-                    for x in tabSplit[9:]:
-                        genotype = getInfoField("GT", infoColumn, x)
-                        genotypeQuality = float(getInfoField("GQ", infoColumn, x))
-                        if genotype != False and genotypeQuality != False:
-                            if genotypePossibilities.get(genotype) == None:
-                                genotypePossibilities[genotype] = float(genotypeQuality)
-                            else:
-                                genotypePossibilities[genotype] += float(genotypeQuality)
-                        else:
-                            genotypeQuality = 0
-                    genotypePossibilities = sorted(genotypePossibilities.iteritems(), key=operator.itemgetter(1), reverse=True)
-                    if len(genotypePossibilities) > 0:
-                        genotype = genotypePossibilities[0][0]
-                        genotypeQuality = genotypePossibilities[0][1]
-                        if len(genotypePossibilities) > 1:
-                            genotypeQuality -= genotypePossibilities[1][1]
-                        alt = ""
-                        if genotype == "0/0" or genotype == "0|0" or genotype == False:
-                            if(len(genotypePossibilities) > 1):
-                                genotype = genotypePossibilities[1][0]
-                                genotypeQuality = 0
-                                
-                        genotypeSplit = re.split("[\|\/]", genotype)
-                        for i in range(len(genotypeSplit)):
-                            
-                        #This is done to ensure the convention of placing the ref allele first
-                        #   in practice, it seems that all VCFs already place the ref first
-                            genotypeSplit[i] = int(genotypeSplit[i])
-                        genotypeSplit.sort()
-    
-                        
-                        alleles = []
-                        for x in genotypeSplit:
-                            if len(alt) > 0:
-                                alt += "/"
-                            alt += altOptions[x]
-                            if x != 0:
-                                alleles.append(altOptions[x])
-                            if len(altOptions[x]) == 0:
-                                alt += "-"
-                    else:
-                        alt = "?"
-                        for x in altOptions:
-                            alt += "/"+x
-                        genotypeQuality = 0
-                        
-                    typeList = []                
-                    #These rules determine how to characterize the type of change that has occurred
-                    for x in alleles:
-                        if len(x) == len(ref) and len(ref) == 1:
-                            type = "SNP"
-                        elif ref in x:
-                            type = "Ins"
-                        elif x in ref:
-                            type = "Del"
-                        else:
-                            type = "Complex"
-                        for x in typeList[1::]:
-                            if typeList[0] != x:
-                                type = "Mixed"
-
-                    hi = lo+len(ref)-1
-                    if len(ref) == 0:
-                        ref = "-"
-                    entry = [chr, lo+overlap-1, hi, type, ref.upper(), alt.upper(), qual, coverage, int(genotypeQuality)]
-                    if storeFullVcf:
-                        entry.append(tabSplit[4])
-                        vcfSpecificData = ''
-                        for x in tabSplit[7:]:
-                            vcfSpecificData += x+"\t"
-                        entry.append(vcfSpecificData.strip())
-                    if type == "Ins" or type == "Complex":
-                        print "Ins"
-                        print entry
-                        print input
-                    #print entry
-                    simpleVar.add_rows([entry])
-                if compressReference:
-                    if priorType == "Ref" and type != priorType:
-                        entry = [chr, priorPosition, lo, type, "", "", 0, 0, 0]
-                        if storeFullVcf:
-                            entry.extend([".", ""])
-                        simpleVar.add_rows([entry])                        
-                if compressNoCall:
-                    if priorType == "No-call" and type != priorType:
-                        entry = [chr, priorPosition, lo, type, "", "", 0, 0, 0]
-                        if storeFullVcf:
-                            entry.extend([".",""])
-                        simpleVar.add_rows([entry])
-                if type != priorType:
-                    priorType = type
-                    priorPosition = lo-1
-        except StopIteration:
-            break
-
-def findMatchingSequence(ref, altOptions, reverse):
-    r = 1
-    if reverse == True:
-        r = -1
-    position = 0
-    minLength = len(ref)
-    for x in altOptions:
-        if len(x[::r]) < minLength:
-            minLength = len(x[::r])
-    for i in range(minLength):
-        for x in altOptions:
-            if ref[::r][i] != x[::r][i]:
-                return i
-    return minLength
-
-def getInfoField(fieldName, infoColumn, infoContents):
-    if infoColumn.count(fieldName) > 0:
-        entrySplitColumn = infoColumn.split(":")
-        position = -1
-        for i in range(len(entrySplitColumn)):
-            if entrySplitColumn[i] == fieldName:
-                position = i
-                entrySplitInfo = infoContents.split(":")
-                if len(entrySplitInfo) == len(entrySplitColumn):
-                    return entrySplitInfo[position]
-    return False
-    
-def generateEmptyList(columns):
-    result = []
-    for i in range(columns):
-        result.append('')
-    return result
     
 def splitGenomeLength(contig_set, includeInterval, excludeInterval, chunkSize, splits):
     details = dxpy.DXRecord(contig_set['$dnanexus_link']).get_details()
